98e201baff2465a1f1a4bb2b06e71bf1613d4eb3
Yavor <yavor300@gmail.com>
2021-02-14 15:29:50 +0200

    added fragments

---
+++ src/main/java/app/linkedout/web/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package app.linkedout.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
