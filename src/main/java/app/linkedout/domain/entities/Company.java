a88724de0f8d3f00ba6f4befcc2c5a53f96b68a2
Yavor <yavor300@gmail.com>
2021-02-14 15:04:06 +0200

    company entity

--- src/main/java/app/linkedout/domain/entities/Company.java
+++ src/main/java/app/linkedout/domain/entities/Company.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "companies")
@@ -13,5 +15,12 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 public class Company extends BaseEntity {
-
+    @Column(nullable = false)
+    private BigDecimal budget;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String town;
 }
